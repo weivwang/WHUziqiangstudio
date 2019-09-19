3577c83552533b7a461c384b3dff9dd3fdcf662e
weivwang <54494072+weivwang@users.noreply.github.com>
2019-09-19 22:06:26 +0800

    修改后的第一次作业

--- package.py
+++ package.py
@@ -18,28 +18,25 @@ if choose == 1:
         s = input('请输入你要加密的字符:')
         a = base64.b64encode(s.encode('Utf-8'))     #实现加密
         print ('加密后的字符为:',a)
-        choose2 = int(input('是否需要解密：4，需要；5，不需要:'))
+        choose2 = int(input('是否需要使用解密功能：4，需要；5，不需要:'))
         if choose2 == 4:
-            b = base64.b64decode(a)   #实现解密
+            d = input('请输入你要解密的字符:')
+            b = base64.b64decode(d)   #实现解密
             print ('解密后的字符为:',b)
     except:
         print('非法输入')    #非法处理
 
 #字典功能
 elif choose == 2:
-    print ('欢迎使用字典功能')
+    print ('欢迎使用字典功能','请注意你输入的key,value之间用逗号连接')
     try:
-        key1 = input('请输入你的第一个key：')
-        value1 = input('请输入你的第一个value：')
-        key2 = input('请输入你的第二个key：')
-        value2 = input('请输入你的第二个value：')
-        key3 = input('请输入你的第三个key：')
-        value3 = input('请输入你的第三个value：')
-        dict1 = {key1 : value1, key2 : value2, key3 : value3} 
+        key1 = input('请输入你的key：')
+        value1 = input('请输入你的value：')
+        dict1 = {key1 : value1} 
         a = type(dict1)
         print ('为你创建的字典为:',dict1,'类型为：',a)
-        L = [key1, key2, key3]
-        R = [value1, value2, value3]
+        L = [key1]
+        R = [value1]
         dict2 = dict(zip(R, L))
         b = type(dict2)
         print('反转后的字典为: ',dict2,'类型为：',b)
@@ -53,25 +50,28 @@ elif choose == 2:
 
 #读取指定文件并生成二维码功能
 elif choose == 3:
-    file1 = input('请输入你要处理的txt文件名：')
-    f = open(file1)
-    d = f.read()
-    print('文件内容为:',d)
+    try:
+        file1 = input('请输入你要处理的txt文件名：')
+        f = open(file1)
+        d = f.read()
+        print('文件内容为:',d)
 
-    def make_qr(str,save):
-        qr=qrcode.QRCode(
-            version = 4,
-            error_correction=qrcode.constants.ERROR_CORRECT_M,
-            box_size=10,
-            border=2,
-        )
-        qr.add_data(str)
-        qr.make(fit=True)
-    
-        img=qr.make_image()
-        img.save(save)
-        save_path = 'txtqrcode.jpg'
-        str = d
+        def make_qr(str,save):
+            qr=qrcode.QRCode(
+                version = 4,
+                error_correction=qrcode.constants.ERROR_CORRECT_M,
+                box_size=10,
+                border=2,
+            )
+            qr.add_data(str)
+            qr.make(fit=True)
+        
+            img=qr.make_image()
+            img.save(save)
+            save_path = 'txtqrcode.jpg'
+            str = d
+    except:
+        print('非法输入')
 print('按enter键退出程序')
 rew = input()
 
